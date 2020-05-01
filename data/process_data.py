48f2426139b00ec89a319ba55dc055891ed45cfe
empyriumz <xindai90@gmail.com>
2020-05-01 17:26:36 -0400

    complete process data script according to jupyternotebook ETL

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,66 @@
 import sys
-
+import pandas as pd
+import numpy as np
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    INPUT
+    file paths of the message and categories files in cvs format
+
+    OUTPUT
+    a dataframe contains both dataset
+    '''
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, how='inner')    
+
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    INPUT
+    a dataframe with both messages and categories for data cleaning
 
+    OUTPUT
+    cleaned dataframe, with new expanding columns for each message category
+    '''
+    # Split `categories` into separate category columns
+    categories = df.categories.str.split(';', expand = True)
+    new_names = pd.Series(categories.loc[0].values).str.split('-', expand = True)[0].values
+    new_names = dict(zip(np.arange(categories.shape[0]), new_names))
+    # rename the new splitted columns
+    categories = categories.rename(columns = new_names)
+
+    # Convert category values to just numbers 0 or 1.
+    for column in categories:
+        # set each value to be the last character of the string
+        # using regex from https://stackoverflow.com/questions/37683558/pandas-extract-number-from-string
+        categories[column] = categories[column].str.extract('(\d+)').astype(int)
+
+    # Replace `categories` column in `df` with new category columns
+    df = df.drop(columns = 'categories')
+    df = pd.concat([df, categories], axis = 1)
+
+    # remove duplicated rows
+    df = df.drop_duplicates(subset = 'id')
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    INPUT
+    cleaned dataframe and the filepath for the SQL database for saving the dataframe
+
+    OUTPUT
+    None
+    '''
+    from sqlalchemy import create_engine
+    engine = create_engine('sqlite:///Raw_data_clean.db')
+    df.to_sql('RawData', engine, if_exists = 'replace', index=False) 
+    engine.dispose()
+
 
 
 def main():
